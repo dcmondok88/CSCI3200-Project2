309a2d89df7732ae3e2dcf77f9b45f508f78ae3c
braeden-baldwin <baldwin.braeden.a@gmail.com>
2025-10-29 12:13:44 -0400

    Copy and Pasted over my static methods
    They will need to be adjusted to fit the requirements, but need the class set up first to do so.

--- CSCI3200/ConvertInfixToPostfix.java
+++ CSCI3200/ConvertInfixToPostfix.java
@@ -6,6 +6,68 @@
 //--------------------------------------------------------------
 package CSCI3200;
 
+import java.util.Stack;
+
 public class ConvertInfixToPostfix {
     
+    public static String convertToPostfix (String input){
+		char current;
+		String result = "";
+		Stack<String> stack = new Stack<String>();
+		
+		
+		for(int i = 0; i < input.length();i++) {
+			current = input.charAt(i);
+			if(checkPrecedence(current)==-1 && !isParenthesis(current) && current!=' ') { //skips over whitespaces
+				result+=(current + " ");
+			}
+			else {
+				if(current=='(') {
+					stack.push(Character.toString(current));
+				}
+				else if (checkPrecedence(current)>0) {
+					while(!stack.isEmpty() && !stack.peek().equals("(") && (checkPrecedence(stack.peek().charAt(0)) >= checkPrecedence(current))) {
+						result+=(stack.pop() + " ");
+					}
+					stack.push(Character.toString(current));
+				}
+				else if (current==')') {
+					while(!stack.isEmpty() && (!(stack.peek().charAt(0)=='('))) {
+						result+=(stack.pop() + " ");
+					}
+					stack.pop();
+				}
+			}
+		}
+		
+		while(!stack.isEmpty()) {
+			result += (stack.pop() + " ");
+		}
+		
+		return result;
+	}
+	
+	public static int checkPrecedence(char operator) {
+        switch (operator) {
+            case '+':
+            case '-':
+                return 1;
+            case '*':
+            case '/':
+            case '%':
+                return 2; 
+            default:
+                return -1; //not a valid operator, can be used to check if the character is an operator
+        }
+	}
+	
+	public static boolean isParenthesis(char paren) {
+		switch(paren) {
+		case '(':
+		case ')':
+			return true;
+		default:
+			return false;
+		}
+	}
 }
