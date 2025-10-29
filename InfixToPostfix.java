8c8e8cf5a8890466adb82771776a0268460982ea
dcmondok88 <dcmondok88@gmail.com>
2025-10-29 11:25:14 -0400

    Added the header that Williams wanted

--- InfixToPostfix.java
+++ InfixToPostfix.java
@@ -1,3 +1,9 @@
+//--------------------------------------------------------------
+//Group Members: Daniel Mondok, Braeden Baldwin & Xavier Alvarenga
+//Date: October 24, 2025
+//Course: Data Structures and Algorithm Analysis (CSCI 3200)
+//Project Description: Project 2 is meant to help convert an infix expression to a postfix expression utilizing a stack as a means to handle operator precedence.
+//--------------------------------------------------------------
 public class InfixToPostfix {
     
 }
